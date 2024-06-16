ce6f1bc731506d2d86593005606def364fa9053b
suraj suryavanshi <93034090+srj393@users.noreply.github.com>
2024-06-16 20:41:49 +0530

    second commit

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,17 +1,16 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
-import User from "@/models/userModels";
 
+connect();
 
-connect()
-
-export default async function POST(request: NextRequest) {
+export async function POST(request: NextRequest) {
     try {
-        const reqBody = await request.json()
-        const {token} = reqBody
-        console.log(token)
-        // 24.21
-    } catch (error:any) {
-        return NextResponse.json({error:error.messageess},{status:500})
+        const reqBody = await request.json();
+        const { token } = reqBody;
+        console.log(token);
+        // Your token handling logic here
+        return NextResponse.json({ message: "Token processed successfully" });
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
